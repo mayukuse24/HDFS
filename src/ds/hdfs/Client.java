e0f0a6d0a398114012dcb79794d3fd85ed1424cb
shaleen <shaleengarg.in@gmail.com>
2016-11-11 22:40:08 +0530

    TT and Client minor change

--- src/ds/hdfs/Client.java
+++ src/ds/hdfs/Client.java
@@ -390,7 +390,7 @@ public class Client
             //FileBytes = FileBytes.concat(BlockResp.getData(0));
 
             try{
-                FileOutputStream fos = new FileOutputStream(FileName, false);
+                FileOutputStream fos = new FileOutputStream(FileName, true);
                 for(ByteString A : BlockResp.getDataList())
                 {
                     String test = A.toStringUtf8();
